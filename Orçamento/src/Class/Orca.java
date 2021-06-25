1ad18f37a1bdad4ab94e4a4fbfce8b95eec32f55
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2021-06-25 10:46:08 -0300

    Add files via upload

--- Orçamento/src/Class/Orca.java
+++ Orçamento/src/Class/Orca.java
@@ -1,25 +1,50 @@
 package Class;
 
-public class Orca {
-	private int id, qtde, frete;
-	private String dia, frn, prod;
-	private double preco;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-	public Orca(int id, int qtde, String dia, String frn, String prod, double preco) {
+public class Orca {
+	private int id, qtde;
+	private Date data;
+	private String frn, prod;
+	private double preco, frete;
+	private SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");// Classe para formatar a Data
+
+	public Orca(int id, int qtde, int data, String frn, String prod, double preco, double frete) {
 		super();
 		this.id = id;
 		this.qtde = qtde;
-		this.dia = dia;
 		this.frn = frn;
 		this.prod = prod;
+		this.frete = frete;
 		this.preco = preco;
 	}
 
-	public int getFrete() {
+	public Orca(Integer valueOf, Date parse, String string, String string2, Integer valueOf2, Integer valueOf3) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public SimpleDateFormat getFormato() {
+		return formato;
+	}
+
+	public void setFormato(SimpleDateFormat formato) {
+		this.formato = formato;
+	}
+
+	public double getFrete() {
 		return frete;
 	}
 
-	public void setFrete(int frete) {
+	public void setFrete(double frete) {
 		this.frete = frete;
 	}
 
@@ -37,14 +62,6 @@ public class Orca {
 
 	public void setQtde(int qtde) {
 		this.qtde = qtde;
-	}
-
-	public String getDia() {
-		return dia;
-	}
-
-	public void setDia(String dia) {
-		this.dia = dia;
 	}
 
 	public String getFrn() {
@@ -75,10 +92,13 @@ public class Orca {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((dia == null) ? 0 : dia.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((formato == null) ? 0 : formato.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(frete);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		result = prime * result + ((frn == null) ? 0 : frn.hashCode());
 		result = prime * result + id;
-		long temp;
 		temp = Double.doubleToLongBits(preco);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		result = prime * result + ((prod == null) ? 0 : prod.hashCode());
@@ -95,10 +115,17 @@ public class Orca {
 		if (getClass() != obj.getClass())
 			return false;
 		Orca other = (Orca) obj;
-		if (dia == null) {
-			if (other.dia != null)
+		if (data == null) {
+			if (other.data != null)
 				return false;
-		} else if (!dia.equals(other.dia))
+		} else if (!data.equals(other.data))
+			return false;
+		if (formato == null) {
+			if (other.formato != null)
+				return false;
+		} else if (!formato.equals(other.formato))
+			return false;
+		if (Double.doubleToLongBits(frete) != Double.doubleToLongBits(other.frete))
 			return false;
 		if (frn == null) {
 			if (other.frn != null)
@@ -135,9 +162,13 @@ public class Orca {
 
 	@Override
 	public String toString() {
-		return "ID "+id+" Produto "+ prod+" Estoque "+qtde+" Frete "+frete+" Dia "+dia+" Fornecedor "+frn+
-				" Preço "+preco+"  " + String.format("%.1f", getSubTotal()) +"\n";
+		return "ID " + id + " Data " + formato.format(data) + " Produto " + prod + " Fornecedor " + frn + " Estoque "
+				+ qtde + " Frete " + frete + " Preço " + preco + "  " + String.format("%.1f", getSubTotal()) + "\n";
 	}
 
-	
+	public String toCSV() {
+		return id + ";" + prod + ";" + qtde + ";" + frete + ";" + formato.format(data) + ";" + frn + ";" + preco + ";"
+				+ String.format("%.1f", getSubTotal()) + "\r\n";
+	}
+
 }
--- Orçamento/src/ClassDAO/OrcaDAO.java
+++ Orçamento/src/ClassDAO/OrcaDAO.java
@@ -1,5 +1,59 @@
 package ClassDAO;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import Class.Orca;
+
 public class OrcaDAO {
 
+	private BufferedWriter bw;
+	private BufferedReader br;
+	private ArrayList<Orca> orcas;
+	private Orca orca;
+	private String program = "./bd/orçamentos.csv";
+	private SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+	public ArrayList<Orca> open() {
+
+		orcas = new ArrayList<>();
+		try {
+			br = new BufferedReader(new FileReader(program));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				String[] pc = line.split(";");
+				orca = new Orca(Integer.valueOf(pc[0]), formato.parse(pc[1]), pc[2], pc[3], Integer.valueOf(pc[4]),
+						Integer.valueOf(pc[5]));
+				orcas.add(orca);
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao abrir arquivo: " + e);
+		} catch (NumberFormatException e) {
+			System.out.println("Erro ao converter números: " + e);
+		} catch (ParseException e) {
+			System.out.println("Erro ao converter datas: " + e);
+		}
+		return orcas;
+	}
+
+	public boolean save(ArrayList<Orca> or) {
+		try {
+			bw = new BufferedWriter(new FileWriter(program, false));
+			for (Orca o : or) {
+				bw.write(o.toCSV());
+			}
+			bw.close();
+			return true;
+		} catch (IOException e) {
+			System.out.println("Erro em gravar o arquivo: " + e);
+		}
+		return false;
+
+	}
 }
